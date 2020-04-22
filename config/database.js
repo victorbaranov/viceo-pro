4f22ed52d6929380b46dde6cb65606f3ec185cb2
victorbaranov <victor.baranov.js@gmail.com>
2020-04-22 11:57:07 +0300

    change url mongo2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI : "mongodb+srv://victoring:ghjcnjdbr777@cluster0-ohy7g.mongodb.net/test?retryWrites=true&w=majority"}
+    module.exports = {mongoURI : "mongodb+srv://victporing:ghjcnjdbr777@vid-pro-4befr.mongodb.net/test?retryWrites=true&w=majority"}
 } else {
     module.exports = {mongoURI : "mongodb://localhost/video-pro"}
 }
