ad478f96ea4a4bd436f83e45768fbaabaa10269f
victorbaranov <victor.baranov.js@gmail.com>
2020-04-24 10:21:04 +0300

    new url12

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ if (process.env.NODE_ENV === 'production') {
     // app.use(express.static('client/build'));
     // app.use(express.static(path.join(__dirname, 'public')))
     app.use(express.static('public'))
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public', 'index.html')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public')));
 }
 
 if (process.env.NODE_ENV === 'development') {
