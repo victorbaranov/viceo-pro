ba7a898d22026fe89aeec88a4ad0fd27ba57a1d7
victorbaranov <victor.baranov.js@gmail.com>
2020-04-24 07:37:41 +0300

    new url2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
-    module.exports = { mongoURI: "mongodb+srv://victporing:ghjcnjdbr777@vid-pro-4befr.mongodb.net/vid-pro?retryWrites=true&w=majority" }
+    module.exports = { mongoURI: "mongodb+srv://victporing:ghjcnjdbr777@vid-pro-4befr.mongodb.net/vidpro?retryWrites=true&w=majority" }
 } else {
     module.exports = { mongoURI: "mongodb://localhost/video-pro" }
 }
