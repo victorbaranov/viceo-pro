93b7c573629e1fe2642003af5d5e090e2c185e5e
victorbaranov <victor.baranov.js@gmail.com>
2020-04-24 08:55:42 +0300

    new url5

--- app.js
+++ app.js
@@ -76,6 +76,12 @@ app.use((req, res, next) => {
     next();
 });
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+  
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+  }
+
 app.get('/', (req, res) => {
     const title = 'Welome from App';
     res.render('index', {
